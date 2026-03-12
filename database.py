a42f23b2b8bd4b63fe9d755875b0ea6cf15dab05
fdaniel-u <229835718+fdaniel-u@users.noreply.github.com>
2026-03-12 03:47:18 -0500

    fix database url railway v3

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL", "")
 # Railway usa postgres://, SQLAlchemy necesita postgresql://
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
